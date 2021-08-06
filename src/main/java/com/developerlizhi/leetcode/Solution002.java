64dc216d6a4fc52e439d6633790ef28a09eab787
developerlizhi <developerlizhi@foxmail.com>
2021-08-06 15:56:26 +0800

    Solution 002 OK

--- src/main/java/com/developerlizhi/leetcode/Solution002.java
+++ src/main/java/com/developerlizhi/leetcode/Solution002.java
@@ -1,33 +1,30 @@
 package com.developerlizhi.leetcode;
 
+
 /**
  * @author developerlizhi
+ * @title leetcode - 002 - 两数相加
+ * @level EASY
+ * @url https://leetcode-cn.com/problems/add-two-numbers/
+ * @solution 模拟
  * @date 2021-04-22 11:32:36
  */
 public class Solution002 {
 
-    //    示例 1：
-    //    输入：l1 = [2,4,3], l2 = [5,6,4]
-    //    输出：[7,0,8]
-    //    解释：342 + 465 = 807.
-    //    示例 2：
-    //
-    //    输入：l1 = [0], l2 = [0]
-    //    输出：[0]
-    //    示例 3：
-    //
-    //    输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-    //    输出：[8,9,9,9,0,0,0,1]
-
     public static void main(String[] args) {
-
         ListNode l1 = new ListNode(3, new ListNode(4, new ListNode(2)));
         ListNode l2 = new ListNode(4, new ListNode(6, new ListNode(5)));
-
-        ListNode head = addTwoNumbers(l1, l2);
-        System.out.println();
+        addTwoNumbers(l1, l2);
     }
 
+    /**
+     * @solution 模拟
+     * @url https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode-solution/
+     * @complexity 时间复杂度：O(n) 空间复杂度：O(1)
+     * 时间复杂度：O(n)，我们要遍历两个链表的全部位置，而处理每个位置只需要 O(1) 的时间。
+     * 空间复杂度：O(1)，没有额外使用空间
+     * @remark 模拟初等数学方法，遍历两个链表，然后注意进位。
+     */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         ListNode head = null, tail = null;
@@ -42,11 +39,11 @@ public class Solution002 {
 
             carry = sum / 10;
 
+            // 第一次循环时，链表为空，将头尾指针均指到第一个结点
             if (head == null) {
-                // 如果为空，则代表头就是尾
                 head = tail = new ListNode(sum % 10);
             } else {
-                // 如果不为空，则新增元素，并修改坐标
+                // 如果不为空，则新增结点，并移动尾指针
                 tail.next = new ListNode(sum % 10);
                 tail = tail.next;
             }
@@ -59,6 +56,8 @@ public class Solution002 {
                 l2 = l2.next;
             }
 
+            // 此处有点难理解，存在进位，新增一个结点，例如 33 + 67 = 101，那么这个进位为 1，就必须要新增一个结点。
+            // 如果是 342 + 465 = 807，这种新增一个结点，其实意义不大，这个节点会被下次循环时被覆盖
             if (carry > 0) {
                 tail.next = new ListNode(carry);
             }
